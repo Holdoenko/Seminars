f56508a6529dcb99b91230de6fc7e26efa4d2f51
Holdoenko <xoldai3005@mail.ru>
2022-11-23 22:08:30 +0300

    Задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача №8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число Num: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int StartNum = 1;
+while (StartNum <= Num)
+    {
+        if (StartNum % 2 == 0)
+        Console.Write("{0}, ",StartNum);
+        StartNum = StartNum + 1;
+    }
